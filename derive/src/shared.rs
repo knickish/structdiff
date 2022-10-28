b80147f94770cf98351783a789f501c95b1bbd16
kirk <knickish@gmail.com>
2022-10-28 01:32:42 +0000

    First pass at recursive diff

--- derive/src/shared.rs
+++ derive/src/shared.rs
@@ -7,3 +7,15 @@ macro_rules! l {
         $target.push_str(&::alloc::format!($line, $($param,)*))
     };
 }
+
+pub fn attrs_recurse(attributes: &[crate::parse::Attribute]) -> bool {
+    attributes
+        .iter()
+        .any(|attr| attr.tokens.len() == 1 && attr.tokens[0] == "recurse")
+}
+
+pub fn attrs_skip(attributes: &[crate::parse::Attribute]) -> bool {
+    attributes
+        .iter()
+        .any(|attr| attr.tokens.len() == 1 && attr.tokens[0] == "skip")
+}
