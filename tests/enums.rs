d8962538f7a670711168da8971a90e28b727e69f
kirk <knickish@gmail.com>
2023-05-06 23:29:53 +0000

    add test for enum derive and trait implementation

---
+++ tests/enums.rs
@@ -0,0 +1,66 @@
+use std::{
+    collections::{BTreeMap, BTreeSet, HashMap},
+    fmt::Debug,
+    num::Wrapping,
+};
+use structdiff::{Difference, StructDiff};
+
+#[cfg(not(feature = "nanoserde"))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, PartialEq, Clone, Difference, Default)]
+pub struct Test {
+    pub test1: i32,
+    pub test2: String,
+    pub test3: Vec<i32>,
+    pub test4: f32,
+    pub test5: Option<usize>,
+}
+
+#[cfg(not(feature = "nanoserde"))]
+#[derive(Debug, PartialEq, Clone, Difference)]
+pub struct TestSkip<A>
+where
+    A: PartialEq,
+{
+    pub test1: A,
+    pub test2: String,
+    #[difference(skip)]
+    pub test3skip: Vec<i32>,
+    pub test4: f32,
+}
+
+#[allow(unused)]
+#[cfg(not(any(feature = "serde", feature = "nanoserde")))]
+#[derive(PartialEq, Difference, Clone)]
+pub enum TestDeriveAllEnum<
+    'a,
+    'b: 'a,
+    A: PartialEq + 'static,
+    const C: usize,
+    B: PartialEq,
+    D,
+    LM: Ord = Option<isize>,
+    const N: usize = 4,
+> where
+    A: core::hash::Hash + std::cmp::Eq + Default,
+    LM: Ord + IntoIterator<Item = isize>,
+    [A; N]: Default,
+    [B; C]: Default,
+    [i32; N]: Default,
+    [B; N]: Default,
+    dyn Fn(&B): PartialEq + Clone + core::fmt::Debug,
+    (dyn std::cmp::PartialEq<A> + Send + 'static): Debug + Clone + PartialEq,
+{
+    F1(()),
+    F2([A; N]),
+    F3([i32; N]),
+    F4(BTreeMap<LM, BTreeSet<<LM as IntoIterator>::Item>>),
+    F5(Option<(A, Option<&'a <LM as IntoIterator>::Item>)>),
+    F6(HashMap<A, BTreeSet<LM>>),
+    F8(BTreeSet<Wrapping<D>>, BTreeSet<Wrapping<B>>),
+    F9 {},
+    F10 { subfield1: u64, subfield2: Test },
+    r#F11(Option<&'b Option<usize>>),
+    F12(TestSkip<A>, TestSkip<A>),
+    F13((TestSkip<A>, TestSkip<B>)),
+}
---
+++ tests/types.rs
@@ -0,0 +1,97 @@
+use generators::{fill, rand_string};
+use nanorand::{Rng, WyRand};
+use structdiff::{Difference, StructDiff};
+
+pub trait RandValue {
+    fn next() -> Self;
+}
+
+#[cfg(not(feature = "nanoserde"))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, PartialEq, Clone, Difference, Default)]
+pub struct Test {
+    pub test1: i32,
+    pub test2: String,
+    pub test3: Vec<i32>,
+    pub test4: f32,
+    pub test5: Option<usize>,
+}
+
+#[cfg(not(feature = "nanoserde"))]
+#[derive(Debug, PartialEq, Clone, Difference)]
+pub struct TestSkip<A>
+where
+    A: PartialEq,
+{
+    pub test1: A,
+    pub test2: String,
+    #[difference(skip)]
+    pub test3skip: Vec<i32>,
+    pub test4: f32,
+}
+
+#[derive(Debug, PartialEq, Clone, Difference, Default)]
+pub enum TestEnum {
+    #[default]
+    F0,
+    F1(()),
+    F2(String),
+    F3 {
+        field1: String,
+        field2: (),
+    },
+    F4(Test),
+}
+
+impl RandValue for Test {
+    fn next() -> Self {
+        let mut rng = WyRand::new();
+        Test {
+            test1: rng.generate(),
+            test2: rand_string(&mut rng),
+            test3: fill(&mut rng),
+            test4: f32::from_bits(rng.generate::<u32>()),
+            test5: match rng.generate::<bool>() {
+                true => Some(rng.generate()),
+                false => None,
+            },
+        }
+    }
+}
+
+impl RandValue for TestEnum {
+    fn next() -> Self {
+        let mut rng = WyRand::new();
+        match rng.generate_range(0..5) {
+            0 => Self::F0,
+            1 => Self::F1(()),
+            2 => Self::F2(rand_string(&mut rng)),
+            3 => Self::F3 {
+                field1: rand_string(&mut rng),
+                field2: (),
+            },
+            _ => Self::F4(Test::next()),
+        }
+    }
+}
+
+mod generators {
+    use nanorand::{Rng, WyRand};
+
+    pub(super) fn rand_string(rng: &mut WyRand) -> String {
+        let base = vec![(); rng.generate::<u8>() as usize];
+        base.into_iter()
+            .map(|_| rng.generate::<u8>() as u32)
+            .filter_map(char::from_u32)
+            .collect::<String>()
+    }
+
+    pub(super) fn fill<V, T>(rng: &mut WyRand) -> T
+    where
+        V: nanorand::RandomGen<nanorand::WyRand, 8>,
+        T: FromIterator<V>,
+    {
+        let base = vec![(); rng.generate::<u8>() as usize];
+        base.into_iter().map(|_| rng.generate::<V>()).collect::<T>()
+    }
+}
